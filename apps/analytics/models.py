d8a54f2cf05192cf1addf02e8b33cca559611250
unknown <kp058665@gmail.com>
2025-07-24 09:52:23 -0400

    bug Fixer

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -2,17 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class VisitLog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    page = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    duration = models.IntegerField(default=0)  # duration in seconds
-
-class VisitLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    path = models.CharField(max_length=255, default='unknown')  # OK
+    path = models.CharField(max_length=255, default='unknown')
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    # Optional fields
     page = models.CharField(max_length=100, null=True, blank=True)
     duration = models.FloatField(null=True, blank=True)
 

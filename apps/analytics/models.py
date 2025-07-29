e88b1a689e6f2015095f959c573d17c1e3facafa
kunal <kp058665@gmail.com>
2025-07-29 00:49:03 -0400

    contact us page

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -12,7 +12,7 @@ class VisitLog(models.Model):
         return f"{self.user} visited {self.path} at {self.timestamp}"
 
 class SearchHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     query = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -27,4 +27,4 @@ class UserActivity(models.Model):
 
 class PopularProduct(models.Model):
     product_name = models.CharField(max_length=255)
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0) 

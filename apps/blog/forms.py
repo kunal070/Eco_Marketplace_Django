3b8db2e93c8415cc3ce72256c97b2a2348d2f7a3
Preshna Patel <128787145+Preshnapatel@users.noreply.github.com>
2025-07-29 00:52:07 -0400

    Updated blog views, URLs, and footer; added migration for SearchHistory model

---
+++ apps/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Newsletter
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your email address'
+            })
+        }
